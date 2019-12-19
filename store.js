3ac06d489181d09048a0dd24196ec56f6b9333c6
malasia woods <malasiawoods@aol.com>
2019-12-18 19:27:04 -0800

    store setup

---
+++ store.js
@@ -0,0 +1,105 @@
+const STORE = {
+    questions: [
+       {
+         question: "Why do cats have whiskers?",
+         options: [
+             "It is extra-grown hair that is a little bit stronger",
+             "To keep bugs off there face",
+             "To see if they can get through a small space"
+         ],
+         answer: "To see if they can get through a small space"
+       },
+
+       {
+         question: "Do cats swivel their ears back?",
+         options: [
+            "No",
+            "yes",
+            "only when noise is made"
+         ],
+         answer: "yes"
+       },
+
+       {
+           question: "Can most cats climb down a tree they got into?",
+           options: [
+               "maybe",
+               "no",
+               "yes"
+           ],
+           answer: "no"
+       },
+
+       {
+           question: "Why can't cats normally climb down trees they get into?",
+           options: [
+               "they don't want to",
+               "they are to afraid",
+               "their claws are curved"
+           ],
+           answer: "their claws are curved"
+       },
+
+       {
+           question: "Are cats nocturnal?",
+           options: [
+               "yes",
+               "no",
+               "only at night"
+           ],
+           answer: "no"
+       },
+
+       {
+           question: "Why do cats have a tail?",
+           options: [
+               "to run fast",
+               "they need it for balance",
+               "they're spine was to long"
+           ],
+           answer: "they need it for balance"
+       },
+
+       {
+           question: "What are haunches?",
+           options: [
+               "shoulders",
+               "lower front legs",
+               "lower back legs"
+           ],
+           answer: "lower back legs"
+       },
+
+       {
+           question: "Why do cats have sandpaper tongues?",
+           options: [
+               "To get meat off of bones",
+               "to clean wounds",
+               "to drink water"
+           ],
+           answer: "to drink water"
+       },
+
+       {
+           question: "Why do cats scratch furniture or trees?",
+           options: [
+               "to hurt your feelings",
+               "Because they don't know any better",
+               "To mark their territory"
+           ],
+           answer: "to mark their territory"
+       },
+
+       {
+           question: "What does it mean when a cat slowly blinks?",
+           options: [
+               "they are relaxed",
+               "they are comfortable",
+               "because they love you"
+           ],
+           answer: "because they love you"
+       }
+    ],
+    currentQuestion: 0,
+    score: 0
+};
